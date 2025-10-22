3f19cb0cf0c60a7e5f0cb436bdc953f3eca39980
Mathieu Desjarlais <github@desmat.ca>
2025-10-22 16:36:59 -0400

    prefer native

--- src/utils/as-array.ts
+++ src/utils/as-array.ts
@@ -1,6 +1,3 @@
-import flatten from 'lodash-es/flatten';
-import compact from 'lodash-es/compact';
-
 export default function asArray(data: any) {
-  return compact(flatten([data]))
+  return [data].flat().filter(Boolean)
 }
