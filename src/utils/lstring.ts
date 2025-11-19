a756d28d0867c7705e3442c2d68732220096f053
Mathieu Desjarlais <github@desmat.ca>
2025-11-19 10:54:11 -0500

    lstring: add preferedlanguages, handle # keys correctly

--- src/utils/lstring.ts
+++ src/utils/lstring.ts
@@ -2,24 +2,28 @@ import { Locales } from "../data/un-languages";
 import type { Locale } from "../types/lstring";
 import type LString from "../types/lstring";
 
-export default function (ltext: string | LString | { [locale: string]: string } | null | undefined, preferedLocale?: Locale): string {
+export default function (ltext?: string | LString | { [locale: string]: string }, ...preferedLocales: Locale[]): string {
   if (!ltext) return "";
 
   if (typeof (ltext) === 'string') return ltext;
 
-  const locale = preferedLocale || "en"; // TODO default to i18n.locale instead ;
+  const cleanedLtext: LString | { [locale: string]: string } = Object.entries(ltext)
+    .filter(isValidLocaleEntry)
+    .reduce((o, [k, v]) => ({ ...o, [k]: v }), {});
+  const ltextLocales = Object.keys(cleanedLtext);
+  const locale = preferedLocales.find((l) => ltextLocales.includes(l)) || "en"; // TODO default to i18n.locale instead ;
 
-  let text = ltext[locale] as string | undefined;
+  let text = cleanedLtext[locale] as string | undefined;
 
   if (!text) {
-    const firstAvailableLocale = Locales.find((l) => !!ltext[l]);
+    const firstAvailableLocale = Locales.find((l) => !!cleanedLtext[l]);
 
-    if (firstAvailableLocale) text = ltext[firstAvailableLocale];
+    if (firstAvailableLocale) text = cleanedLtext[firstAvailableLocale];
   }
 
   if (!text) {
     // cannot find any of the un locales..... return first text value
-    text = Object.values(ltext)
+    text = Object.values(cleanedLtext)
       .filter(o => typeof (o) === 'string')
       .filter(o => !!o)[0];
   }
@@ -29,14 +33,26 @@ export default function (ltext: string | LString | { [locale: string]: string } 
   return text;
 }
 
-export function trim(ltext: LString) {
+export function trim(ltext?: string | LString | { [locale: string]: string }) {
+  if (!ltext) return ltext;
+
+  if (typeof (ltext) === 'string') return ltext.trim();
+
   return Object.entries(ltext).reduce((ret, [k, v]) => ({ ...ret, [k]: v?.trim() }), {});
 }
 
-export function isNullOrEmpty(ltext?: LString) {
-  return !ltext || !Object.values(ltext).some(Boolean);
+export function isNullOrEmpty(ltext?: string | LString | { [locale: string]: string }) {
+  return !ltext || !Object.entries(ltext)
+    .filter(isValidLocaleEntry)
+    .some(([k, v]) => !!v);
 }
 
-export function isNullOrWhiteSpace(ltext?: LString) {
-  return !ltext || !Object.values(trim(ltext)).some(Boolean);
+export function isNullOrWhiteSpace(ltext?: string | LString | { [locale: string]: string }) {
+  return !ltext || !Object.entries(trim(ltext))
+    .filter(isValidLocaleEntry)
+    .some(([k, v]) => !!v);
+}
+
+function isValidLocaleEntry([k, v]: any) {
+  return k && !k.startsWith("#")
 }
